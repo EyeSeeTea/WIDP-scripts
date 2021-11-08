e699803f4cf4f7b770f5a5e7437cb081646ffa8d
Alexis Rico <sferadev@gmail.com>
2021-11-08 13:00:57 +0100

    Update and rename check_values.sql to check_orgunit_values.sql

--- events_jsonb_manipulation/check_orgunit_values.sql
+++ events_jsonb_manipulation/check_orgunit_values.sql
@@ -2,8 +2,7 @@ select * from datavalue where sourceid in (select organisationunitid from organi
 
 select * from datavalueaudit where organisationunitid in (select organisationunitid from organisationunit where uid in ("uid1","uid2")):
 
-
- select * from trackedentitydatavalueaudit where programstageinstanceid in (select programstageinstanceid from programstageinstance where organisationunitid in (select organisationunitid from organisationunit where uid in ("uid1","uid2")));
+select * from trackedentitydatavalueaudit where programstageinstanceid in (select programstageinstanceid from programstageinstance where organisationunitid in (select organisationunitid from organisationunit where uid in ("uid1","uid2")));
  
 select * from programinstance where organisationunitid in ("uid1","uid2");
 
@@ -14,9 +13,10 @@ select * from trackedentityinstance where organisationunitid in ("uid1","uid2");
 select * from trackedentityprogramowner where organisationunitid in ("uid1","uid2"); //not sure about what does this table.
 
 select * from datavalue where dataelementid in (select dataelementid from dataelement where valuetype='ORGANISATION_UNIT') and value in ("uid1","uid2"):
+
 select * from datavalueaudit where dataelementid in (select dataelementid from dataelement where valuetype='ORGANISATION_UNIT') and value in ("uid1","uid2"):
 
-SELECT foo.* FROM (SELECT uid AS event, json.key AS dataElement, jsonb_extract_path_text(json.value, 'value') AS orgUnit
+SELECT query.* FROM (SELECT uid AS event, json.key AS dataElement, jsonb_extract_path_text(json.value, 'value') AS orgUnit
 FROM programstageinstance, jsonb_each(programstageinstance.eventdatavalues) AS json
-WHERE json.key IN (select uid from dataelement where valuetype='ORGANISATION_UNIT')) AS foo
-WHERE foo.orgUnit IN ('uid1', 'uid2');
+WHERE json.key IN (select uid from dataelement where valuetype='ORGANISATION_UNIT')) AS query
+WHERE query.orgUnit IN ('uid1', 'uid2');
