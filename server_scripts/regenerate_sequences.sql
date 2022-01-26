3b4abcec788124fb96f5e6af71d2b736a5ce3cc1
Alexis Rico <sferadev@gmail.com>
2022-01-26 11:42:14 +0100

    Create regenerate_sequences.sql

---
+++ server_scripts/regenerate_sequences.sql
@@ -0,0 +1,44 @@
+create sequence if not exists reservedvalue_sequence;
+select setval('reservedvalue_sequence', max(reservedvalueid)) FROM reservedvalue;
+
+create sequence if not exists potentialduplicatesequence;
+select setval('potentialduplicatesequence', max(potentialduplicateid)) FROM potentialduplicate;
+
+create sequence if not exists audit_auditid_seq;
+select setval('audit_auditid_seq', max(auditid)) FROM audit;
+
+create sequence if not exists datavalueaudit_sequence;
+select setval('datavalueaudit_sequence', max(datavalueauditid)) FROM datavalueaudit;
+
+create sequence if not exists programstageinstance_sequence;
+select setval('programstageinstance_sequence', max(programstageinstanceid)) FROM programstageinstance;
+
+create sequence if not exists trackedentitydatavalueaudit_sequence;
+select setval('trackedentitydatavalueaudit_sequence', max(trackedentitydatavalueauditid)) FROM trackedentitydatavalueaudit;
+
+create sequence if not exists trackedentityinstance_sequence;
+select setval('trackedentityinstance_sequence', max(trackedentityinstanceid)) FROM trackedentityinstance;
+
+create sequence if not exists trackedentityinstanceaudit_sequence;
+select setval('trackedentityinstanceaudit_sequence', max(trackedentityinstanceauditid)) FROM trackedentityinstanceaudit;
+
+create sequence if not exists programinstance_sequence;
+select setval('programinstance_sequence', max(programinstanceid)) FROM programinstance;
+
+create sequence if not exists deletedobject_sequence;
+select setval('deletedobject_sequence', max(deletedobjectid)) FROM deletedobject;
+
+create sequence if not exists reservedvalue_sequence;
+select setval('reservedvalue_sequence', max(reservedvalueid)) FROM reservedvalue;
+
+create sequence if not exists usermessage_sequence;
+select setval('usermessage_sequence', max(usermessageid)) FROM usermessage;
+
+create sequence if not exists messageconversation_sequence;
+select setval('messageconversation_sequence', max(messageconversationid)) FROM messageconversation;
+
+create sequence if not exists message_sequence;
+select setval('message_sequence', max(messageid)) FROM message;
+
+create sequence if not exists trackedentityprogramowner_sequence;
+select setval('trackedentityprogramowner_sequence', max(trackedentityprogramownerid)) FROM trackedentityprogramowner;
